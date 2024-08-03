5e79942f35c7e06aa71db557ba14bbf329ec6be8
AntonSamojlow <anton.samojlow@gmail.com>
2024-08-03 19:48:15 +0000

    updated SHA512 for current test

--- vcpkg-port/portfile.cmake
+++ vcpkg-port/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AntonSamojlow/cpp-aux
-    REF 0.0.1
-    SHA512 0
+    REF 0.0.1-alpha
+    SHA512 d0ccc3de80ae446b0357db3b775ee654f983825bb076053b777e5e731d8894a33b421f8e5a797df1cb9af14fd3ad173d195560f4df17e230535d7f948be55642
     HEAD_REF master
 )
 
