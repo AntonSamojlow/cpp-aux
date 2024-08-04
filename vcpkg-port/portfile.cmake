35a0d90e3802e5b45119ba80b66244d258a826aa
AntonSamojlow <anton.samojlow@gmail.com>
2024-08-04 18:11:52 +0200

    add BUILD_TESTS option

--- vcpkg-port/portfile.cmake
+++ vcpkg-port/portfile.cmake
@@ -2,11 +2,15 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO AntonSamojlow/cpp-aux
     REF 0.0.1-alpha
-    SHA512 e2f98d9f39be3863d1a94c712e0a9220c077ec48c6c6f3b37e0b06e012e7195d6f994bdb06e28119c6aa148c3d9a9d408da7d5efe36c7adebe1812b05501a60d
+    SHA512 faa453d3dfda35074177ff4fe39c8358c076d085ba56a889110bc1f70db1a30aaabfdfac589f62a8260d362648d66537c865aedf2db2f6e3f2f31ebf6a955e6b
     HEAD_REF master
 )
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
-vcpkg_cmake_install()
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+)
+# vcpkg_cmake_install()
 # todo:
 # file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/libogg" RENAME copyright)
